f73e6cd3e5aae01334add651140120cd63012837
flo-mic <florianmichel@hotmail.de>
2022-01-28 17:22:04 +0100

    fix sed command

--- docker_start.sh
+++ docker_start.sh
@@ -5,9 +5,9 @@ if [[ -n "${CLAMAV_ADDRESS}" ]]; then
     if [[ -z "${CLAMAV_PORT}" ]]; then
         CLAMAV_PORT=3310
     fi
-    sed -i "s/plugin_clamav_connect_type=socket/plugin_clamav_connect_type=tcp" antivirus-plugin/plugins/antivirus-config.conf
-    sed -i "s/plugin_clamav_address=127.0.0.1/plugin_clamav_address=${CLAMAV_ADDRESS}" antivirus-plugin/plugins/antivirus-config.conf
-    sed -i "s/plugin_clamav_address=127.0.0.1/plugin_clamav_address=${CLAMAV_PORT}" antivirus-plugin/plugins/antivirus-config.conf
+    sed -i "s/plugin_clamav_connect_type=socket/plugin_clamav_connect_type=tcp/g" antivirus-plugin/plugins/antivirus-config.conf
+    sed -i "s/plugin_clamav_address=127.0.0.1/plugin_clamav_address=${CLAMAV_ADDRESS}/g" antivirus-plugin/plugins/antivirus-config.conf
+    sed -i "s/plugin_clamav_address=127.0.0.1/plugin_clamav_address=${CLAMAV_PORT}/g" antivirus-plugin/plugins/antivirus-config.conf
     mv antivirus-plugin/plugins/* coreruleset/plugins/
     cp coreruleset/nginx-modsecurity-plugins.conf coreruleset/nginx-modsecurity.conf
 fi
@@ -23,17 +23,17 @@ fi
 # Add crs-setup file
 echo "Include /etc/nginx/owasp-modsecurity-crs/crs-setup.conf" > coreruleset/nginx-modsecurity.conf
 # Add plugin conf files if present
-if [[ $(ls -1 coreruleset/plugins/*-config.conf | wc -w) > 0  ]]; then
-    echo "/etc/nginx/owasp-modsecurity-crs/plugins/*-config.conf" >> coreruleset/nginx-modsecurity.conf
+if [[ $(find coreruleset/plugins -name '*-before.conf' | wc -l) != 0  ]]; then
+    echo "Include /etc/nginx/owasp-modsecurity-crs/plugins/*-config.conf" >> coreruleset/nginx-modsecurity.conf
 fi
 # Add plugin *-before.conf files if present
-if [[ $(ls -1 coreruleset/plugins/*-before.conf | wc -w) > 0  ]]; then
+if [[ $(find coreruleset/plugins -name '*-before.conf' | wc -l) != 0  ]]; then
     echo "Include modsecurity.d/owasp-modsecurity-crs/plugins/*-before.conf" >> coreruleset/nginx-modsecurity.conf
 fi
 # Add core rule set rules
 echo "Include /etc/nginx/owasp-modsecurity-crs/rules/*.conf" >> coreruleset/nginx-modsecurity.conf
 # Add plugin *-before.conf files if present
-if [[ $(ls -1 coreruleset/plugins/*-after.conf | wc -w) > 0  ]]; then
+if [[ $(find coreruleset/plugins -name '*-before.conf' | wc -l) != 0  ]]; then
     echo "Include modsecurity.d/owasp-modsecurity-crs/plugins/*-after.conf" >> coreruleset/nginx-modsecurity.conf
 fi
 
