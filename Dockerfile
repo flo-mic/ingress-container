1f889e4ff449cec655cf678be8bb045cdcc76062
flo-mic <florianmichel@hotmail.de>
2022-01-28 16:12:31 +0100

    add first plugins

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,9 @@ RUN apk update && \
     # Get required files
     git clone -b ${OWASP_VERSION} --depth 1 --quiet https://github.com/coreruleset/coreruleset && \
     mv coreruleset/crs-setup.conf.example coreruleset/crs-setup.conf && \
+    git clone https://github.com/coreruleset/antivirus-plugin && \
+    mv antivirus-plugin/plugins/antivirus-config.conf.example antivirus-plugin/plugins/antivirus-config.conf && \
+    git clone https://github.com/coreruleset/fake-bot-plugin && \
     # Start cleanup
     apk del --purge build-dependencies && \
     rm -rf \
@@ -21,6 +24,5 @@ RUN apk update && \
 COPY docker_start.sh /docker_start.sh
 COPY nginx-modsecurity.conf coreruleset/nginx-modsecurity.conf
 COPY modsecurity.conf modsecurity.conf
-COPY nginx.tmpl nginx.tmpl
 
 ENTRYPOINT [ "/docker_start.sh" ]
--- docker_start.sh
+++ docker_start.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# Configure antivirus plugin
+if [[ -z "${CLAMAV_ADDRESS}"]]; then
+    if [[ -z "${CLAMAV_PORT}" ]]; then
+        CLAMAV_PORT=3310
+    fi
+    sed -i "s/plugin_clamav_connect_type=socket/plugin_clamav_connect_type=tcp" antivirus-plugin/plugins/antivirus-config.conf
+    sed -i "s/plugin_clamav_address=127.0.0.1/plugin_clamav_address=${CLAMAV_ADDRESS}" antivirus-plugin/plugins/antivirus-config.conf
+    sed -i "s/plugin_clamav_address=127.0.0.1/plugin_clamav_address=${CLAMAV_PORT}" antivirus-plugin/plugins/antivirus-config.conf
+    mv antivirus-plugin/plugins/* /coreruleset/plugins/
+fi
+
+# Configure fakebot google plugin
+if [[ ${FAKEBOT_PLUGIN_ENNABLED} = "true"]]; then
+    mv fake-bot-plugin/plugins/* /coreruleset/plugins/
+fi
+
 cp -r coreruleset/* /etc/nginx/owasp-modsecurity-crs/
-cp modsecurity.conf /etc/nginx/modsecurity/modsecurity.conf
-cp nginx.tmpl /etc/nginx/template/nginx.tmpl
+#cp modsecurity.conf /etc/nginx/modsecurity/modsecurity.conf
+
+
+
+
+    
