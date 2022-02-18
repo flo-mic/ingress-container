a9deae13750075bb4ce305d4f62f20a1d64234ae
flo-mic <florianmichel@hotmail.de>
2022-02-18 18:39:06 +0100

    add secruleengine as env

--- images/init-container/docker_start.sh
+++ images/init-container/docker_start.sh
@@ -77,7 +77,14 @@ fi
 if [[ -d "/etc/nginx/owasp-modsecurity-crs/" ]]; then
     echo "Create nginx-modsecurity configuration file"
 
+    # Add modsec conf file to loading directive
     echo "Include /etc/nginx/modsecurity/modsecurity.conf" > /etc/nginx/owasp-modsecurity-crs/nginx-modsecurity.conf
+    
+    # Define default SecRuleAction
+    if [[ -n "${SEC_RULE_ENGINE}" ]]; then
+        echo "SecRuleEngine ${SEC_RULE_ENGINE}" >> /etc/nginx/owasp-modsecurity-crs/nginx-modsecurity.conf
+    fi
+        
     echo "Include /etc/nginx/owasp-modsecurity-crs/crs-setup.conf" >> /etc/nginx/owasp-modsecurity-crs/nginx-modsecurity.conf
     # Add plugin conf files if present
     for line in $(find /etc/nginx/owasp-modsecurity-crs/plugins -name '*-config.conf' | sort); do 
